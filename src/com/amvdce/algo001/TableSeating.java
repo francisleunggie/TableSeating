aacd005a3c17eed87de09932ffb72eefb09441e2
francisleunggie <francisleunggie@gmail.com>
2016-10-19 23:41:30 +0800

    added TableSeating Project

---
+++ src/com/amvdce/algo001/TableSeating.java
@@ -0,0 +1,261 @@
+/**
+ * 
+ */
+package com.amvdce.algo001;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Hashtable;
+import java.util.Vector;
+
+/**
+ * @author Francis
+ *
+ */
+/**
+ * @author Francis
+ *
+ */
+public class TableSeating {
+	private int[] probs;
+	
+	/**
+	 * find the max number of contiguous unfilled tables
+	 * @param arr
+	 * @return
+	 */
+	protected int maxFalse(boolean[] arr) {
+		int max = 0;
+		int streak = 0;
+		for (boolean i:arr) {
+			if (!i) {
+				streak++;
+			} 
+			max = streak > max?streak:max;
+			if (i) streak = 0;
+		}
+		return max;
+	}
+	
+	/**
+	 * Count the number of unfilled tables
+	 * @param arr
+	 * @return
+	 */
+	protected int countFalse(boolean[] arr) {
+		int output = 0;
+		for (boolean i:arr) {
+			if (!i) output++;
+		}
+		return output;
+	}
+	
+	/**
+	 * Count the number of filled tables
+	 * @param arr
+	 * @return
+	 */
+	protected int countTrue(boolean[] arr) {
+		int output = 0;
+		for (boolean i:arr) {
+			if (i) output++;
+		}
+		return output;
+	}
+	
+	/**
+	 * Check if the customer can be seated
+	 * @param tables
+	 * @param pos
+	 * @param tabPerCust
+	 * @return
+	 */
+	protected boolean canSit(boolean[] tables, int pos, int tabPerCust) {
+		if (pos + tabPerCust > tables.length) return false;
+		boolean[] tableSub = Arrays.copyOfRange(tables, pos, pos+tabPerCust);
+		if (countFalse(tableSub) < tableSub.length) return false;
+		return true;		
+	}
+	
+	
+	/**
+	 * Count how many sits are available for the current per cust size 
+	 * @param tables
+	 * @param tabPerCust
+	 * @return
+	 */
+	protected int countSitable(boolean[] tables, int tabPerCust) {
+		int count = 0;
+		for (int i = 0; i < tables.length; i++ ){
+			if (canSit(tables, i, tabPerCust)) count++;
+		}
+		return count;		
+	}
+	
+	/**
+	 * Return all the available table numbers 
+	 * @param tables
+	 * @param tabPerCust
+	 * @return
+	 */
+	protected Vector<Integer> getSitable(boolean[] tables, int tabPerCust) {
+		Vector<Integer> list = new Vector<Integer>();
+		for (int i = 0; i < tables.length; i++ ){
+			if (canSit(tables, i, tabPerCust)) list.add(i);
+		}
+		return list;		
+	}
+	
+	/**
+	 * Sit the customer
+	 * @param tables
+	 * @param pos
+	 * @param tabPerCust
+	 * @return
+	 */
+	protected boolean[] sitCust (boolean[] tables, int pos, int tabPerCust) {
+		boolean[] output = new boolean[tables.length];
+		for (int i=0; i<tables.length; i++) {
+			output[i] = tables[i];
+			if (i >= pos && i < pos + tabPerCust) {
+				output[i] = true;
+			}
+		}
+		return output;
+	}
+	
+	/**
+	 * Calculate the eventual number of tables filled at the lowest leave of the inverted tree
+	 * @param currID
+	 * @param output
+	 * @param expected
+	 * @param sitables
+	 * @param tabPerCust 
+	 * @return
+	 */
+	protected double updateFilled(String currID, double output, double expected, Vector<Integer> sitables, int tabPerCust) {
+		int denominator = sitables.isEmpty()? 1:sitables.size();
+		//System.out.println("["+ currID +"].avail="+ sitables +",o="+output + "+=("+expected+" x "+(this.probs[tabPerCust-1]/100.0)+ ")/"+denominator+", = "+(output + (expected*(this.probs[tabPerCust-1]/100.0)/denominator)));
+		output += (this.probs[tabPerCust-1]/100.0)*(expected/denominator);
+		return output;
+	}
+	
+	/**
+	 * At every turn, find the number of filled given the current probability 
+	 * and the current customer we are working on
+	 * @param tables - non-nullable
+	 * @param currCust - starts with 0
+	 * @param tabPerCust - table per customer
+	 * @return
+	 */
+	public double getExpected(boolean[] tables, int currCust, int tabPerCust, String pId) {
+		double output = 0.0;
+		Vector<Integer> sitables = getSitable(tables, tabPerCust);
+		int sitable = sitables.size();
+		// at the lowest leaf of the inverted tree, calculate the eventual filled 
+		if (sitable < 1) {
+			String chosen = sitable == 0? "":String.valueOf(sitables.get(0));
+			int additon = sitable == 0?0:tabPerCust;
+			String currID = pId+"-"+currCust+""+chosen;
+			output = updateFilled(currID, output, countTrue(tables)+additon, sitables, tabPerCust);
+			return output;			
+		} else {			
+			
+			//for the current customer, find out if he can sit starting from a particular position,
+			// if yes, sit him, and try to sit the next customer. 
+			// if this continues till all customers have been seated, count filled.
+			// if this ends prematurely, i.e. currCust < numTables, just count filled
+			for (int i=0; i < sitables.size(); i++) {				
+				int table = sitables.get(i);
+				
+				//loop for each possible num of tables to be occupied by the next customer 
+				for (int j=0; j < probs.length; j++) {
+					int cTabPerCust = j+1;
+					String currID = pId+"_"+tabPerCust+"-"+table;
+					boolean[] afterSitting = sitCust(tables, table, tabPerCust);
+					double expected = 0.0;
+					if (probs[j] == 0 || cTabPerCust > maxFalse(afterSitting)) {
+						expected = countTrue(afterSitting);
+					} else {
+						expected = getExpected(afterSitting, currCust+1, cTabPerCust, currID);
+					}
+					output = updateFilled(currID, output, expected, sitables, cTabPerCust);
+				}
+			}
+			
+		}
+		return output;
+	}
+	
+	/**
+	 * At every turn, find the number of filled given the current probability 
+	 * and the current customer we are working on
+	 * @param tables - non-nullable
+	 * @param currCust - starts with 0
+	 * @param tabPerCust - table per customer
+	 * @return
+	 */
+//	public double getExpected(boolean[] tables, int currCust, int tabPerCust, String pId) {
+//		double output = 0.0;
+//		Vector<Integer> sitables = getSitable(tables, tabPerCust);
+//		int sitable = sitables.size();
+//		// at the lowest leaf of the inverted tree, calculate the eventual filled 
+//		if (sitable < 1) {
+//			String chosen = sitable == 0? "":String.valueOf(sitables.get(0));
+//			int additon = sitable == 0?0:tabPerCust;
+//			String currID = pId+"-"+currCust+""+chosen;
+//			output = updateFilled(currID, output, countTrue(tables)+additon, sitables);
+//			return output;			
+//		} else {			
+//			//for the current customer, find out if he can sit starting from a particular position,
+//			// if yes, sit him, and try to sit the next customer. 
+//			// if this continues till all customers have been seated, count filled.
+//			// if this ends prematurely, i.e. currCust < numTables, just count filled
+//			for (int i=0; i < sitables.size(); i++) {				
+//				int table = sitables.get(i);
+//				String currID = pId+"-"+currCust+""+table;
+//				double expected = getExpected(sitCust(tables, table, tabPerCust), currCust+1, tabPerCust, currID);				
+//				output = updateFilled(currID, output, expected, sitables);
+//			}
+//			
+//		}
+//		return output;
+//	}
+	
+	
+	
+	/** main function
+	 * 	
+	 * 	
+	 * 12
+	 * {9,9,9,9,9,9,9,9,9,9,10}
+	 * Returns: 7.871087929710551
+	 * 
+	 * 
+	 * At every turn, find the number of filled as of the current probability
+	 * @param numTables
+	 * @param probs
+	 * @return the number of tables filled
+	 */
+	
+	public double getExpected(int numTables, int[] probs) {
+		double output = 0.0;
+		this.probs = probs;
+		//iterate through the number of probs
+		for (int tabPerCust = 0; tabPerCust < probs.length; tabPerCust++) {
+			if (probs[tabPerCust] == 0 || tabPerCust > numTables) continue;
+			Date start = new Date();
+			System.out.print("Start - "+ start);
+			double temp = 0.0;
+			temp = getExpected(new boolean[numTables], 0, tabPerCust+1, "s-"+(tabPerCust+1));
+			output += temp*probs[tabPerCust];
+			//} else output += numTables*probs[tabPerCust];
+			Date end = new Date();
+			System.out.println(", took " + (end.getTime() - start.getTime()) + " ms: Main: output now at "+ output +", tabPerCust=" + (tabPerCust+1) + ", end at " + end);
+		}
+		output /= 100;
+		System.out.println("Final Output: "+ numTables +"="+output);
+		return output;
+	}
+
+}
---
+++ src/com/amvdce/algo001/TestTableSeating.java
@@ -0,0 +1,136 @@
+/**
+ * 
+ */
+package com.amvdce.algo001;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Vector;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Francis
+ *
+ */
+public class TestTableSeating extends TestCase {
+	private static final double DELTA = 1e-9;
+	private int numbTable; 
+	private int[] probs;
+	@Override
+	protected void setUp() throws Exception {
+		numbTable = 12;
+		probs = new int[]{0,0,0,0,0,50,50};
+	}
+	
+	public void testCountTrue() {
+		TableSeating tS = new TableSeating();
+		assertTrue(tS.countTrue(new boolean[]{true, true, false, false}) == 3);
+	}
+	
+	public void testCanSit() {
+		TableSeating tS = new TableSeating();
+		boolean [] tables = new boolean[]{true, true, false, false, false};
+		int pos = 2;
+		int tabPerCust = 3;
+		assertTrue(tS.canSit(tables, pos, tabPerCust));
+	}
+	
+	public void testSitCust() {
+		TableSeating tS = new TableSeating();
+		boolean [] tables = new boolean[]{true, true, false, false, false};
+		int pos = 2;
+		int tabPerCust = 3;
+		assertTrue(tS.countTrue(tS.sitCust(tables, pos, tabPerCust)) == 4);
+	}
+	
+	public void testSitable() {
+		TableSeating tS = new TableSeating();
+		boolean [] tables = new boolean[]{false, false, false, false};
+		int tabPerCust = 2;
+		assertTrue(tS.countSitable(tables, tabPerCust) == 4);
+	}
+	
+	public void testGetsitable() {
+		TableSeating tS = new TableSeating();
+		boolean [] tables = new boolean[]{false, false, false, false};
+		List<Integer> list = Arrays.asList(0,1,3);
+		int tabPerCust = 2;
+		assertTrue(tS.getSitable(tables, tabPerCust).equals(new Vector<Integer>(list)));
+	}
+	
+	public void testGetExpectedChild() {
+		TableSeating tS = new TableSeating();
+		boolean [] tables = new boolean[4];
+		// first, work on 
+		/*
+		 * 	4
+		 *	{0,100}
+		 *	Returns: 3.3333333333333335
+		 * prob[1]: 2 table per cust, output should be sum(4, 2, 4, 0) = 10/(4-1) 
+		 */
+		double expected = tS.getExpected(tables, 0, 2, null);
+		assertEquals(expected, (double)(10.0/3), DELTA);
+		
+	}
+
+	public void testGetExpected() {
+		TableSeating tS = new TableSeating();
+		
+		int[] probs = new int[]{0,100};
+		// first, work on 
+		/*
+		 * 	4
+		 *	{0,100}
+		 *	Returns: 3.3333333333333335
+		 * prob[1]: 2 table per cust, output should be sum(4, 2, 4, 0) = 10/(4-1) 
+		 */
+		double expected = (double)(10.0/3);
+		double actual = tS.getExpected(4,new int[]{100});
+//		actual = tS.getExpected(4,probs);
+//		assertEquals(actual, expected, DELTA);
+//		probs = new int[]{0,0,0,0,0,50,50};
+//		expected = tS.getExpected(5,probs);
+//		assertEquals(expected, (double)(0.0), DELTA);
+//		probs = new int[]{0,100};
+//		actual = tS.getExpected(6,probs);
+//		expected = (32.0/3 + 8 + 6 )/5;
+//		assertEquals(actual, expected, DELTA);
+		/*
+		 * 	Main: output now at 52.28267852227195, tabPerCust=1
+		 *  Main: output now at 105.54640601979484, tabPerCust=2
+		 *  Main: output now at 161.11162023313688, tabPerCust=3
+		 *  Main: output now at 219.58624350037366, tabPerCust=4
+		 *  Main: output now at 282.0091821050748, tabPerCust=5
+		 *  Main: output now at 348.9051713149552, tabPerCust=6
+		 *  Main: output now at 421.2117757810553, tabPerCust=7
+		 *  Main: output now at 499.3756319710553, tabPerCust=8
+		 *  Main: output now at 584.2458929710554, tabPerCust=9
+		 *  Main: output now at 676.2087929710553, tabPerCust=10
+		 *  Main: output now at 787.1087929710553, tabPerCust=11
+		 *  Final Output: 12=7.871087929710553
+		 */
+		probs = new int[]{9,9,9,9,9,9,9,9,9,9,10};
+		expected = tS.getExpected(12,probs);
+		assertEquals(expected, (double)(7.871087929710551), DELTA);
+		
+	}
+	
+	public void testMaxUnfilled() {
+		TableSeating tS = new TableSeating();
+		
+		boolean[] tables = new boolean[]{false, false, true, false, false, false, true, true, false, false, false, false};
+		int expected = 4;
+		int actual = tS.maxFalse(tables);
+		assertEquals(actual , expected);
+	}
+	
+	public void testPrintTime() {
+		Date start = new Date();
+		System.out.print("Start - "+start);
+		Date end = new Date();
+		System.out.println("different: " + (end.getTime() - start.getTime()));
+	}
+}
